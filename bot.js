2e6a7f825e904573a3cb11f27802b8aa57aa6b74
jaiShyamji <jaishreeshyamji077@gmail.com>
2026-09-11 12:23:47 +0530

    bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,33 @@
 require('dotenv').config();
 const { Bot } = require('grammy');
 const config = require('./config');
-const h = require('./handlers');
+const m = require('./menuHandlers');
+const o = require('./orderHandlers');
 
 if (!config.BOT_TOKEN) process.exit(1);
 const bot = new Bot(config.BOT_TOKEN);
 
-bot.command("start", h.start);
-bot.callbackQuery("back_to_menu", h.backMenu);
-bot.callbackQuery("main_services", h.servicesMenu);
-bot.callbackQuery("p_tg", h.tgMenu);
-bot.callbackQuery("p_ig", h.igMenu);
-bot.callbackQuery("p_fb", h.fbMenu);
-bot.callbackQuery("p_yt", h.ytMenu);
-bot.hears(/^\/\d+$/, h.handleSlashCode);
-bot.callbackQuery(/^q_\d+_(.+)$/, h.handleQtyButtons);
-bot.callbackQuery("main_add_funds", h.addFundsMenu);
-bot.callbackQuery(/^pay_(via_upi|via_usdt)$/, h.initPayMethod);
-bot.callbackQuery("p_done", h.payDone);
-bot.callbackQuery("main_orders", h.ordersHistory);
-bot.callbackQuery("toggle_currency", h.toggleCurrency);
-bot.on("message:text", h.handleTextMessages);
+bot.command("start", m.start);
+bot.callbackQuery("back_to_menu", m.backMenu);
+bot.callbackQuery("check_balance", m.checkBalance);
+bot.callbackQuery("my_profile", m.myProfile);
+bot.callbackQuery("my_channels", m.myChannels);
+bot.callbackQuery("main_promo", m.mainPromo);
+bot.callbackQuery("main_support", m.mainSupport);
+bot.callbackQuery("toggle_currency", m.toggleCurrency);
+
+bot.callbackQuery("main_services", o.servicesMenu);
+bot.callbackQuery("p_tg", o.tgMenu);
+bot.callbackQuery("p_ig", o.igMenu);
+bot.callbackQuery("p_fb", o.fbMenu);
+bot.callbackQuery("p_yt", o.ytMenu);
+bot.hears(/^\/\d+$/, o.handleSlashCode);
+bot.callbackQuery(/^q_\d+_(.+)$/, o.handleQtyButtons);
+bot.callbackQuery("main_add_funds", o.addFundsMenu);
+bot.callbackQuery(/^pay_(via_upi|via_usdt)$/, o.initPayMethod);
+bot.callbackQuery("p_done", o.payDone);
+bot.callbackQuery("main_orders", o.ordersHistory);
+bot.on("message:text", o.handleTextMessages);
 
 bot.start();
-console.log("Super Clean System Active!");
+console.log("HAPPY REACTION Full Architecture Bot Started Live!");
