365d08ee806ce843e4b3598173b5fe923fb8fcc5
jaiShyamji <jaishreeshyamji077@gmail.com>
2026-09-11 14:48:47 +0000

    config.js

--- config.js
+++ config.js
@@ -1,21 +1,19 @@
 module.exports = {
-module.exports = {
     BOT_TOKEN: process.env.BOT_TOKEN,
     SMM_API_URL: process.env.SMM_API_URL || "https://smmlite.com",
     SMM_API_KEY: process.env.SMM_API_KEY,
     SUPPORT_USERNAME: process.env.SUPPORT_USERNAME || "YourSupportUsername",
     
-    // 👑 ADMIN SETTINGS
+    // ADMIN SETTINGS
     ADMIN_ID: 7991401218, 
     USD_TO_INR_RATE: 95.0,
 
-    // 🇮🇳 UPI MANUAL PAYMENT SETTINGS
-    UPI_ID: "shyamji@ybl", // Yahan apni UPI ID dalo bhai
+    // UPI MANUAL PAYMENT SETTINGS
+    UPI_ID: "prince2026gupta@okicici",
     MERCHANT_NAME: "HAPPY REACTION", 
-    // Link ekdum perfect kar diya hai bhai chart waala:
     UPI_QR_LINK: "https://googleapis.com",
 
-    // 🪙 USDT MANUAL PAYMENT SETTINGS
-    USDT_ADDRESS: "TYxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // Yahan apna USDT address dalo bhai
+    // USDT MANUAL PAYMENT SETTINGS
+    USDT_ADDRESS: "TYxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     USDT_QR_LINK: "https://googleapis.com"
 };
