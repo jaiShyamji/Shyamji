2ed11b8e0367460613234f5aee20f3675b64834d
jaiShyamji <jaishreeshyamji077@gmail.com>
2026-09-13 08:33:35 +0000

    config.js

--- config.js
+++ config.js
@@ -4,16 +4,15 @@ module.exports = {
     SMM_API_KEY: process.env.SMM_API_KEY,
     SUPPORT_USERNAME: process.env.SUPPORT_USERNAME || "YourSupportUsername",
     
-    // ADMIN SETTINGS
+    // 👑 ADMIN & MANUAL PAYMENT CONFIG
     ADMIN_ID: 7991401218, 
     USD_TO_INR_RATE: 95.0,
 
-    // UPI MANUAL PAYMENT SETTINGS
-    UPI_ID: "prince2026gupta@okicici",
+    // 🇮🇳 TUNHARI LIVE UPI SETTINGS
+    UPI_ID: "prince2026gupta@okicici", 
     MERCHANT_NAME: "HAPPY REACTION", 
-    UPI_QR_LINK: "https://googleapis.com",
 
-    // USDT MANUAL PAYMENT SETTINGS
-    USDT_ADDRESS: "TYxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    USDT_QR_LINK: "https://googleapis.com"
+    // 🪙 USDT MULTI-NETWORK ADDRESSES
+    USDT_TRC20: "TYxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // TRC20 Address yahan dalo bhai
+    USDT_BEP20: "0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  // BEP20 Address yahan dalo bhai
 };
